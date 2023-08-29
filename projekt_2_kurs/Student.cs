ae0082e96ff43d5e0d818603ce3b2ea00bef9dfc
opilane <opilane@HP-CZC2349HTQ>
2023-08-29 16:27:38 +0300

    Add project files.

---
+++ projekt_2_kurs/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    public class Student
+    {
+        public string Name { get; }
+        public int age;
+        private readonly string _city;
+        public string status;
+        public int Year;
+        public Student(string name, int age, string city, int year)
+        {
+            Name = name;
+            Age = age;
+            _city = city;
+            YEar = year;
+        }
+        public int YEar
+        {       
+           get { year} 
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                if (age < 18) status = "alaealine";
+                else status = "täiskasvanud";
+            }
+        }
+
+        public string GetCity()
+        {
+            return _city;
+        }
+
+        public void showInfo()
+        {
+        Console.WriteLine(Name);
+        Console.WriteLine(Age);
+        Console.WriteLine(GetCity());
+    }
+}
+
+    
+
---
+++ projekt_2_kurs/StudentMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class StudentMain
+    {
+        public static void Main()
+        {
+            Student student = new Student("John", 17, "London");
+
+        //Console.WriteLine(student.Name);
+        //Console.WriteLine(student.Age);
+        //Console.WriteLine(student.GetCity());
+        student.showInfo();
+
+        }
+    }
